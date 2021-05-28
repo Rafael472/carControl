c04e4288ac272ec9eafb2ed92839ee86a07a4b7a
Rafael da Silva <rafael._silva@outlook.com.br>
2021-05-28 13:09:31 -0300

    Configuração do Projeto

---
+++ src/main/java/com/orangeTalents/carControl/CarControlApplication.java
@@ -0,0 +1,13 @@
+package com.orangeTalents.carControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangeTalents/carControl/CarControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangeTalents.carControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
